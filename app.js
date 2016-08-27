640414a124d359cfd6467b2d587784f0ead576ad
louisdante <mase2kool@yahoo.com.au>
2016-08-28 00:30:14 +0100

    creates a method for session creation and deletion

--- app.js
+++ app.js
@@ -6,7 +6,25 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     methodOverride = require('method-override'),
     session = require('express-session'),
+    _ = require('lodash'),
     firebase = require("firebase");
+
+
+//init firebase
+var config = {
+    apiKey: "AIzaSyCfXrpBKVGKKFIZ8MOnreNtdtLrMtHGcII",
+    authDomain: "pair-programming-cea9c.firebaseapp.com",
+    databaseURL: "https://pair-programming-cea9c.firebaseio.com",
+    storageBucket: "pair-programming-cea9c.appspot.com",
+};
+
+//init firebase
+firebase.initializeApp(config);
+
+//init realtime database
+var db = firebase.database().ref("session-id");
+
+
 
 // var config = require('./config.js'), //config file contains all tokens and other private info
 //    funct = require('./functions.js'); //funct file contains our helper functions for our Passport and database work
@@ -23,8 +41,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(methodOverride('X-HTTP-Method-Override'));
 
-
-//  write a middleware that protectes te app route
+app.use('/signin', function(req, res, next) {
+        if (!req.session.username) {
+            next();
+        } else {
+            res.redirect("/home/" + req.session.username);
+        }
+    })
+    //  write a middleware that protectes te app route
 function authChecker(req, res, next) {
     if (req.session.username) {
         //res.redirect("/home/" + req.session.username);
@@ -81,16 +105,29 @@ app.post('/signin', function(req, res) {
 });
 
 //displays our app page
-app.get('/home/:email', authChecker, function(req, res) {
-    console.log(req.session, 'testing');
+app.get('/home', authChecker, function(req, res) {
     sess = req.session;
-    console.log(sess.username);
     var user = sess.username;
-    console.log(user, 'luubuibsi');
     res.render('home', { user: user });
 });
 
-//logs user out of site, deleting them from the session, and returns to homepage
+
+//create new session route
+app.get('/session/:session', function(req, res) {
+    var user = req.session.username;
+    console.log(user);
+    var session = req.params.session;
+    var dbs = firebase.database().ref("session-id");
+    dbs.on('value', function(snapshot) {
+        console.log('got yere');
+        var page_sess = _.values(snapshot.val());
+        var someActive = _.filter(page_sess, { session: 'javascript' })[0];
+        console.log(someActive);
+        res.render('home', { user: user, session: session });
+    });
+});
+
+
 app.get('/logout', function(req, res) {
     var sess = req.session;
     var user = sess.username
